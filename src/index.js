9b016e4c39ee789575f2db7c034ee45eb474449b
HaikaNoor <68729246+HaikaNoor@users.noreply.github.com>
2020-11-17 11:54:53 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <div>
     Hello World
-  </div>
+  </div>,
   document.getElementById('root')
 );
 
